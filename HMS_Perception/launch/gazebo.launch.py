945cc000f622b6c5f1c07a7c45d8182bdd8b8229
Chunho Li <lchli@ucdavis.edu>
2026-03-10 00:29:27 +0000

    LAUNCH FILE WORKING

--- HMS_Perception/launch/gazebo.launch.py
+++ HMS_Perception/launch/gazebo.launch.py
@@ -1,48 +1,27 @@
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch_ros.substitutions import FindPackageShare
 import os
 
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from ros_gz_bridge.actions import RosGzBridge
+from ros_gz_sim.actions import GzServer
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='HMS_Perception').find('HMS_Perception')
-    default_model_path = os.path.join(
-        pkg_share, 'src', 'description', 'HMS_Perception_description.sdf'
-    )
+    pkg_share = get_package_share_directory('HMS_Perception')
+    ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
+    gz_spawn_model_launch_source = os.path.join(ros_gz_sim_share, "launch", "gz_spawn_model.launch.py")
+    default_model_path = os.path.join(pkg_share, 'src', 'description', 'HMS_Perception_description.sdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+    world_path = os.path.join(pkg_share, 'world', 'mars_world.sdf')
+    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{
-            # Read the SDF file contents directly (no xacro).
-            'robot_description': ParameterValue(
-                Command(['cat ', LaunchConfiguration('model')]),
-                value_type=str,
-            )
-        }]
-    )
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{
-            'robot_description': ParameterValue(
-                Command(['cat ', LaunchConfiguration('model')]),
-                value_type=str,
-            )
-        }],
-        condition=UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
     rviz_node = Node(
         package='rviz2',
@@ -51,25 +30,37 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
+    gz_server = GzServer(
+        world_sdf_file=world_path,
+        container_name='ros_gz_container',
+        create_own_container='True',
+        use_composition='True',
+    )
+    ros_gz_bridge = RosGzBridge(
+        bridge_name='ros_gz_bridge',
+        config_file=bridge_config_path,
+        container_name='ros_gz_container',
+        create_own_container='False',
+        use_composition='True',
+    )
+    spawn_entity = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_spawn_model_launch_source),
+        launch_arguments={
+            'world': 'mars_world',
+            'topic': '/robot_description',
+            'entity_name': 'HMS_Perception',
+            'z': '0.65',
+        }.items(),
+    )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='gui',
-            default_value='True',
-            description='Flag to enable joint_state_publisher_gui'
-        ),
-        DeclareLaunchArgument(
-            name='model',
-            default_value=default_model_path,
-            description='Absolute path to robot model file (SDF)'
-        ),
-        DeclareLaunchArgument(
-            name='rvizconfig',
-            default_value=default_rviz_config_path,
-            description='Absolute path to rviz config file'
-        ),
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        DeclareLaunchArgument(name='use_sim_time', default_value='True', description='Flag to enable use_sim_time'),
+        DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen'),
         robot_state_publisher_node,
-        rviz_node
+        rviz_node,
+        gz_server,
+        ros_gz_bridge,
+        spawn_entity,
     ])
